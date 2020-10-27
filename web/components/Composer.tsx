025950be986d7b5e6a87dbee61d35f5a6cd4d3e2
tolfino <tolfino@campsite.social>
2020-10-27 12:29:45 -0700

    hook up composer

--- web/components/Composer.tsx
+++ web/components/Composer.tsx
@@ -1,20 +1,32 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import TextareaAutosize from 'react-autosize-textarea';
 
 import { useTranslation } from '../i18n';
 import Avatar from './Avatar';
 import styles from './Composer.module.css';
 
-const Composer = memo(({ }: {}) => {
+interface PostSkeleton {
+    content: string;
+}
+
+const Composer = memo(({ onSubmit }: { onSubmit: (skel: PostSkeleton) => void }) => {
     const [t, i18n] = useTranslation('composer');
+
+    const [content, setContent] = useState('');
 
     return <div className={styles['composer']}>
         <div className={styles['container']}>
             <div className={styles['rail']}>
                 <a className={styles['avatar']} href='/'><Avatar url='https://upload.wikimedia.org/wikipedia/commons/c/cd/Portrait_Placeholder_Square.png' size='2.5rem' /></a>
             </div>
-            <form className={styles['body']}>
-                <TextareaAutosize className={styles['content']} placeholder={t('write-something')} />
+            <form className={styles['body']} onSubmit={(e) => {
+                e.preventDefault();
+                onSubmit({
+                    content: content,
+                });
+                setContent('');
+            }}>
+                <TextareaAutosize className={styles['content']} placeholder={t('write-something')} value={content} onChange={e => setContent((e.target as HTMLTextAreaElement).value)} />
                 <div className={styles['controls']}>
                     <button type="submit">{t('post')}</button>
                 </div>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -5,8 +5,9 @@ import Composer from '../components/Composer';
 import { PostChildren } from '../components/Thread';
 import Thread from '../components/Thread';
 import * as modelsPb from '../gen/proto/campsite/v1/models_pb';
+import * as postsPb from '../gen/proto/campsite/v1/posts_pb';
 import * as topicsPb from '../gen/proto/campsite/v1/topics_pb';
-import { topicsClient } from '../lib/rpc';
+import { postsClient, topicsClient } from '../lib/rpc';
 
 export default function Index() {
     const [pubs, setPubs]: [modelsPb.Publication[], Dispatch<SetStateAction<modelsPb.Publication[]>>] = useState([]);
@@ -33,8 +34,16 @@ export default function Index() {
             <title>Campsite</title>
         </Head>
         <div>
-            <Composer />
-            {pubs.map(pub => <Thread tree={{ post: pub.getPost(), children: PostChildren() }} maxChildDepth={0} collapsible={true} onShowMoreChildren={() => {}} key={pub.getPost().getId()}></Thread>)}
+            <Composer onSubmit={(skel) => {
+                const req = new postsPb.CreatePostRequest();
+                req.setContent(skel.content);
+                const call = postsClient.createPost(req, {
+                    authorization: 'Bearer W8CNKPQBSPaFr5kfn-GJxw',
+                }, (err, resp) => {
+                    console.log(resp);
+                });
+            }} />
+            {pubs.map(pub => <Thread tree={{ post: pub.getPost(), children: PostChildren() }} maxChildDepth={0} collapsible={true} onShowMoreChildren={() => { }} key={pub.getPost().getId()}></Thread>)}
         </div>
     </div>;
 }
